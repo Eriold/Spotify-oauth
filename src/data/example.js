a6484f56110d102c7d3c0f9acf08c783fcb38bfb
Eriold <dacamoty@gmail.com>
2020-11-18 13:27:40 -0500

    going to last version 16 react

---
+++ src/data/example.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+    title: 'Dogs',
+    author: 'Ramstei',
+    duration: 125,
+    album: 'biber',
+    img: './assets/chiwawa.jpg'
+}
